fa83e27126ffab670c6882c203f6d876d1fab961
vadym312 <vadym.maksymuik@outlook.com>
2025-06-17 17:25:45 +0300

    fixed build issue

--- app/blog/[slug]/layout.tsx
+++ app/blog/[slug]/layout.tsx
@@ -1,23 +1,32 @@
 import { Metadata } from "next";
 import he from "he";
+import { ResolvingMetadata } from 'next';
 
-export async function generateMetadata({ params }: { params: { slug: string } }): Promise<Metadata> {
+type Props = {
+  children: React.ReactNode;
+  params: Promise<{ slug: string }>;
+}
+
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const resolvedParams = await params;
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://www.nextmotion.net';
-  const post = await fetch(`${baseUrl}/api/post?slug=${params.slug}`).then(res => res.json());
+  const post = await fetch(`${baseUrl}/api/post?slug=${resolvedParams.slug}`).then(res => res.json());
   
   return {
     title: he.decode(post.metaTitle || post.title.rendered),
     description: he.decode(post.metaDescription || post.excerpt.rendered.replace(/<[^>]*>/g, '')),
     alternates: {
-      canonical: `${baseUrl}/blog/${params.slug}`,
+      canonical: `${baseUrl}/blog/${resolvedParams.slug}`,
     },
   };
 }
 
 export default function BlogPostLayout({
   children,
-}: {
-  children: React.ReactNode;
-}) {
+  params,
+}: Props) {
   return children;
 } 
