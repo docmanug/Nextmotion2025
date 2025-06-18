c4d2760ec28bda6c28df813a74797d6228a42dc4
vadym312 <vadym.maksymuik@outlook.com>
2025-06-18 16:51:51 +0300

    resolved the build issue

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -20,7 +20,7 @@ async function getCategories(): Promise<Category[]> {
 
 type Props = {
 	params: Promise<{ slug: string }>
-	searchParams: { [key: string]: string | string[] | undefined }
+	searchParams: Promise<{ [key: string]: string | string[] | undefined }>
 }
 
 export async function generateStaticParams() {
