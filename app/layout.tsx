658460cf655c4ba001840afee23d18aa934fb5c1
vadym312 <vadym.maksymuik@outlook.com>
2025-09-13 11:34:12 +0100

    added google tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { Suspense } from "react";
 import { LoadingSkeleton } from "@/components/ui/LoadingSkeleton";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   title:
@@ -174,19 +175,39 @@ export default async function RootLayout({
           fbq('track', 'PageView');
         `}
       </Script>
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-PCK4QN5X');
+        `}
+      </Script>
       <head>
         <HreflangTags />
         <noscript>
-          <img height="1" width="1" style={{ display: 'none' }}
+          <Image
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            alt="Facebook Pixel"
             src="https://www.facebook.com/tr?id=24288009300898224&ev=PageView&noscript=1"
           />
         </noscript>
       </head>
       <body>
+        <noscript>
+          <iframe
+            title="Google Tag Manager"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PCK4QN5X"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         <Navbar />
-        <Suspense fallback={<LoadingSkeleton />}>
-          {children}
-        </Suspense>
+        <Suspense fallback={<LoadingSkeleton />}>{children}</Suspense>
         <Footer />
       </body>
     </html>
